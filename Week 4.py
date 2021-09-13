ca8f6cf190ad1d40560177a9fb779eef21b44b6b
itsak10 <89334924+itsak10@users.noreply.github.com>
2021-09-13 22:05:05 +0530

    Add files via upload

---
+++ Week 4.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# __ADITYA KOSHTI__
+# 
+# I am currently studying Bsc. in Data Science.
+
+# _I am interested in data science because i like to find the hidden patterns_
+
+# ### The code in the 5 cell is going to perform subtraction .
+
+# In[13]:
+
+
+print(99-33)
+
+
+# #Bulleted list
+#  - 1
+#     - 2
+#     
+# #Numbered list
+# 
+# 1. A
+# 1. N
+# 
+# #Table
+# 
+# | Test | Positive/Negative | Accuracy |
+# | ---- | ----------------- | -------- |
+# | Corona | Positive | 90% |
+# | HIV | Negative | 97% |
+# 
+# 
+# 
+# 
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
